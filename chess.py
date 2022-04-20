1b154d7dc3f00e62689e7787c1778bba36dd0b6b
Kavin Bharathi <66783110+cipherDOT@users.noreply.github.com>
2022-04-20 20:48:05 +0530

    changed color theme, added responsive sprites

--- chess.py
+++ chess.py
@@ -5,29 +5,30 @@ pygame.init()
 
 file = 8
 rank = 8
-board = [[0 for _ in range(rank)] for _ in range(file)]
-width = 400
-height = 400
+width = 800
+height = 800
+rez = width // file
 display = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Chess")
 font = pygame.font.Font("./assets/custom_fonts/Poppins/Poppins-Light.ttf", 24)
 peice_image = pygame.image.load("./assets/pieces.png").convert_alpha()
 
-def clip(surface, x, y, x_size, y_size):
+def clip(surface, x, y, x_size, y_size, _rez):
     handle_surface = surface.copy()
     clipRect = pygame.Rect(x,y,x_size,y_size)
     handle_surface.set_clip(clipRect)
     image = surface.subsurface(handle_surface.get_clip())
-    image = pygame.transform.scale(image, (50, 50))
-    # pygame.image.save(image, f"{name}.png")
+    image = pygame.transform.scale(image, (_rez, _rez))
     return image
 
 
 class Color():
     White = (255, 255, 255)
     Black = (0, 0, 0)
-    Light = (185, 123, 101)
-    Dark = (100, 59, 46)
+
+    # chess.com blues theme
+    Light = (123,162,192)  # light blue tiles
+    Dark = (76,125,165)    # dark blue tiles
     Invert = {
         White : Black,
         Black : White,
@@ -37,20 +38,20 @@ class Color():
 
 class Piece():
     # White Pieces
-    K = clip(peice_image, 0, 0, 333, 333)
-    Q = clip(peice_image, 333, 0, 333, 333)
-    B = clip(peice_image, 666, 0, 333, 333) 
-    N = clip(peice_image, 999, 0, 333, 333)
-    R = clip(peice_image, 1333, 0, 333, 333)
-    P = clip(peice_image, 1666, 0, 333, 333)
+    K = clip(peice_image, 0, 0, 333, 333, rez)
+    Q = clip(peice_image, 333, 0, 333, 333, rez)
+    B = clip(peice_image, 666, 0, 333, 333, rez) 
+    N = clip(peice_image, 999, 0, 333, 333, rez)
+    R = clip(peice_image, 1333, 0, 333, 333, rez)
+    P = clip(peice_image, 1666, 0, 333, 333, rez)
 
     # Black Pieces
-    k = clip(peice_image, 0, 333, 333, 333)
-    q = clip(peice_image, 333, 333, 333, 333)
-    b = clip(peice_image, 666, 333, 333, 333)
-    n = clip(peice_image, 999, 333, 333, 333)
-    r = clip(peice_image, 1333, 333, 333, 333)
-    p = clip(peice_image, 1666, 333, 333, 333)
+    k = clip(peice_image, 0, 333, 333, 333, rez)
+    q = clip(peice_image, 333, 333, 333, 333, rez)
+    b = clip(peice_image, 666, 333, 333, 333, rez)
+    n = clip(peice_image, 999, 333, 333, 333, rez)
+    r = clip(peice_image, 1333, 333, 333, 333, rez)
+    p = clip(peice_image, 1666, 333, 333, 333, rez)
 
 fen_to_peice = {
     # White Pieces
@@ -98,18 +99,10 @@ class ChessBoard(object):
                 state =  str(self.board[file][rank])
                 if state.isalpha():
                     piece = fen_to_peice[state]
-                    # piece = pygame.transform.scale(piece, (self.rez, self.rez))
                     display.blit(piece, (rank * self.rez, file * self.rez))
                 else:
                     blank = font.render(' ', 1, Color.Black)
                     display.blit(blank, (rank * self.rez, file * self.rez))
-
-                # pygame.display.update((
-                #     rank * self.rez,
-                #     file * self.rez, 
-                #     rank * self.rez + self.rez, 
-                #     file * self.rez + self.rez
-                # ))
 
     def fen_to_board(self):
         valid = self.fen.count('/') == 7
@@ -139,7 +132,7 @@ class ChessBoard(object):
 
 def main():
     run = True
-    initial_fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR"
+    initial_fen = "r1b1k1nr/p2p1pNp/n2B4/1p1NP2P/6P1/3P1Q2/P1P1K3/q5b1"
     chess_board = ChessBoard((0, 0), initial_fen, width // 8)
 
     while run:
